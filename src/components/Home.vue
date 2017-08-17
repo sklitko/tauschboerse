174142f7f045cf68f969034ee348fb3211ce49dc
klitko <zheltovodets@ukr.net>
2017-08-17 14:13:01 +0300

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <div class="col-md-12 col-xs-12">
+            <h1>Hello App!</h1>
+            <router-link tag="button" class="btn btn-lg" to="/list">List</router-link>
+            <router-link tag="button" class="btn btn-lg" to="/new">New</router-link>
+        </div>
+    </div>
+</template>
---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="col-md-6 col-xs-12">
+    <div class="test">
+      <h1>{{ msg }}</h1>
+      <h2>Test</h2>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      msg: 'LIST Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+body {
+  background-color: brown;
+}
+
+</style>
