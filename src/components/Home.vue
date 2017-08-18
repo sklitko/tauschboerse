6b7430b6e3e920478f0a5128a48da03496adaaf3
klitko <zheltovodets@ukr.net>
2017-08-18 09:14:08 +0300

    add link to home add form to list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,16 @@
 <template>
-    <div class="container">
-        <div class="col-md-12 col-xs-12">
-            <h1>Hello App!</h1>
-            <router-link tag="button" class="btn btn-lg" to="/list">List</router-link>
-            <router-link tag="button" class="btn btn-lg" to="/new">New</router-link>
-        </div>
-    </div>
+  <div>
+    <b-nav>
+      <b-nav-item to="/list">List</b-nav-item>
+      <b-nav-item to="/new">New</b-nav-item>
+    </b-nav>
+  </div>
 </template>
+
+<style>
+
+  body {
+    background-color: white;
+  }
+
+</style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,30 +1,69 @@
 <template>
-  <div class="container">
-    <div class="col-md-6 col-xs-12">
-    <div class="test">
-      <h1>{{ msg }}</h1>
-      <h2>Test</h2>
-    </div>
+  <div class="container-fluid">
+    <div class="col-xs-12">
+      <div class="filtering">
+      <h1>Filter</h1>
+        <div class="form-group">
+          <label for="search" class="search-label">Search</label>
+          <input type="text" id="search">
+        </div>
+          <div class="form-group">
+            <label for="category">Category</label>
+            <b-form-select id="category" v-model="selected_cat" :options="category" ></b-form-select>
+          </div>
+        <div class="form-group">
+          <label for="location">Category</label>
+          <b-form-select id="location" v-model="selected_loc" :options="location" ></b-form-select>
+        </div>
+      </div>
+
+      <div class="list">
+        <ul>
+          <li v-for="feed in feedData.data">{{feed.number}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import feedData from '../feed.json';
+
 export default {
   name: 'list',
-  data () {
+  data() {
     return {
-      msg: 'LIST Vue.js App'
+      selected_cat: null,
+      category: [
+        { value: null, text: '--select--' },
+        { value: 'Search', text: 'Search' },
+        { value: 'Gift', text: 'Gift' }
+      ],
+      selected_loc: null,
+      location: [
+        { value: null, text: '--select--'},
+        { value: 'Point A', text: 'Point A'},
+        { value: 'Point B', text: 'Point B'},
+        { value: 'Point C', text: 'Point C'},
+        { value: 'Point D', text: 'Point D'},
+        { value: 'Point E', text: 'Point E'},
+      ],
+      feedData
     }
   }
 }
+
+console.log(feedData.data);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 
-body {
-  background-color: brown;
+.filtering {
+  background-color: sandybrown;
+  color: #fff;
+  padding: 20px;
 }
+
 
 </style>
